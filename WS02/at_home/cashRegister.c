dba6d2415382c7be658d3800146a42e03f034bfc
Cameron Gray <cfgray72@gmail.com>
2018-01-26 19:11:32 -0500

    Workshop #2 - Released

---
+++ WS02/at_home/cashRegister.c
@@ -0,0 +1,9 @@
+/*
+     Name:      <your name> 
+     Student#:  <your student number>
+     Section:   <your course section letter>
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+// Paste your in-lab code below and upgrade it to the at-home specifications
