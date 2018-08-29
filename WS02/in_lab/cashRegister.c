0e2d12d158c6795913975d9ab661e2f7a4f27b35
Cameron Gray <cfgray72@gmail.com>
2018-08-28 20:35:10 -0400

    Reset 2018-Fall

--- WS02/in_lab/cashRegister.c
+++
@@ -1,9 +0,0 @@
-/*
-     Name:      <your name> 
-     Student#:  <your student number>
-     Section:   <your course section letter>
-*/
-
-#define _CRT_SECURE_NO_WARNINGS
-
-// Start your code below:
