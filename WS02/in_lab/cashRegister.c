c9bbd57985565620674340931063eaffbf0d4ddd
Cameron Gray <cfgray72@gmail.com>
2018-09-09 14:41:00 -0400

    Workshop #2 Released

---
+++ WS02/in_lab/cashRegister.c
@@ -0,0 +1,9 @@
+/*
+     Name:      <your name> 
+     Student#:  <your student number>
+     Section:   <your course section letter>
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+// Start your code below:
