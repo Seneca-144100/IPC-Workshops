69190045bada99635985b965c6328096c7dc7f59
Cameron Gray <cfgray72@gmail.com>
2018-09-23 14:08:33 -0400

    Workshop 4 - Released

---
+++ WS04/in_lab/temps2.c
@@ -0,0 +1,11 @@
+//--------------------------------------------------
+// Name:           
+// Student Number: 
+// Email:          
+// Section:        
+// Workshop:       
+//--------------------------------------------------
+
+#define _CRT_SECURE_NO_WARNINGS
+
+// Place your code below
