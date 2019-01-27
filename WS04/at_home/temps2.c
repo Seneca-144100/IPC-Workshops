074f871a02ff4b8caaa6c6ffa11ade52441c23a6
Cameron Gray <cameron.gray@senecacollege.ca>
2019-01-26 19:04:23 -0500

    Workshop 4 - Released

---
+++ WS04/at_home/temps2.c
@@ -0,0 +1,11 @@
+//--------------------------------------------------
+// Name:           Full name here
+// Student Number: #########
+// Email:          userID@myseneca.ca
+// Section:        XXX
+// Workshop:       4 (at-home)
+//--------------------------------------------------
+
+#define _CRT_SECURE_NO_WARNINGS
+
+// Place your code below
