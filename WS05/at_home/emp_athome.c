07db7f5cef8e224409b7196b747b310204f05a0c
Cameron Gray <cameron.gray@senecacollege.ca>
2019-02-03 11:55:27 -0500

    Workshop #5: Released

---
+++ WS05/at_home/emp_athome.c
@@ -0,0 +1,239 @@
+//--------------------------------------------------
+// Name:           Full name here
+// Student Number: #########
+// Email:          userID@myseneca.ca
+// Section:        XXX
+// Workshop:       5 (at-home)
+//--------------------------------------------------
+
+
+// Copy your source code from in_lab file to here
+// Expand the option list to include option 3 & 4 after 
+// option 2. as following
+// "3. Update Employee Salary"
+// "4. Remove Employee"
+
+// Create two switch-cases for option 3 & 4 after case 2. 
+// Implement "Update Salary" and "Remove Employee" 
+// functionality as per instructions
+// inside the relevant case statements
+
+
+
+
+
+
+
+// PROGRAM OUTPUT IS SHOW BELOW
+
+/*
+---=== EMPLOYEE DATA ===---
+
+1. Display Employee Information
+2. Add Employee
+3. Update Employee Salary
+4. Remove Employee
+0. Exit
+
+Please select from the above options: 5
+
+ERROR: Incorrect Option: Try Again
+
+1. Display Employee Information
+2. Add Employee
+3. Update Employee Salary
+4. Remove Employee
+0. Exit
+
+Please select from the above options: 1
+
+EMP ID  EMP AGE EMP SALARY
+======  ======= ==========
+
+1. Display Employee Information
+2. Add Employee
+3. Update Employee Salary
+4. Remove Employee
+0. Exit
+
+Please select from the above options: 2
+
+Adding Employee
+===============
+Enter Employee ID: 222
+Enter Employee Age: 22
+Enter Employee Salary: 22222.22
+
+1. Display Employee Information
+2. Add Employee
+3. Update Employee Salary
+4. Remove Employee
+0. Exit
+
+Please select from the above options: 2
+
+Adding Employee
+===============
+Enter Employee ID: 333
+Enter Employee Age: 33
+Enter Employee Salary: 33333.33
+
+1. Display Employee Information
+2. Add Employee
+3. Update Employee Salary
+4. Remove Employee
+0. Exit
+
+Please select from the above options: 2
+
+Adding Employee
+===============
+Enter Employee ID: 444
+Enter Employee Age: 44
+Enter Employee Salary: 44444.44
+
+1. Display Employee Information
+2. Add Employee
+3. Update Employee Salary
+4. Remove Employee
+0. Exit
+
+Please select from the above options: 2
+
+Adding Employee
+===============
+Enter Employee ID: 555
+Enter Employee Age: 55
+Enter Employee Salary: 55555.55
+
+1. Display Employee Information
+2. Add Employee
+3. Update Employee Salary
+4. Remove Employee
+0. Exit
+
+Please select from the above options: 2
+
+Adding Employee
+===============
+ERROR!!! Maximum Number of Employees Reached
+
+1. Display Employee Information
+2. Add Employee
+3. Update Employee Salary
+4. Remove Employee
+0. Exit
+
+Please select from the above options: 1
+
+EMP ID  EMP AGE EMP SALARY
+======  ======= ==========
+222       22   22222.22
+333       33   33333.33
+444       44   44444.44
+555       55   55555.55
+
+1. Display Employee Information
+2. Add Employee
+3. Update Employee Salary
+4. Remove Employee
+0. Exit
+
+Please select from the above options: 3
+
+Update Employee Salary
+======================
+Enter Employee ID: 123
+*** ERROR: Employee ID not found! ***
+Enter Employee ID: 321
+*** ERROR: Employee ID not found! ***
+Enter Employee ID: 333
+The current salary is 33333.33
+Enter Employee New Salary: 99999.99
+
+1. Display Employee Information
+2. Add Employee
+3. Update Employee Salary
+4. Remove Employee
+0. Exit
+
+Please select from the above options: 1
+
+EMP ID  EMP AGE EMP SALARY
+======  ======= ==========
+222       22   22222.22
+333       33   99999.99
+444       44   44444.44
+555       55   55555.55
+
+1. Display Employee Information
+2. Add Employee
+3. Update Employee Salary
+4. Remove Employee
+0. Exit
+
+Please select from the above options: 4
+
+Remove Employee
+===============
+Enter Employee ID: 789
+*** ERROR: Employee ID not found! ***
+Enter Employee ID: 987
+*** ERROR: Employee ID not found! ***
+Enter Employee ID: 333
+Employee 333 will be removed
+
+1. Display Employee Information
+2. Add Employee
+3. Update Employee Salary
+4. Remove Employee
+0. Exit
+
+Please select from the above options: 1
+
+EMP ID  EMP AGE EMP SALARY
+======  ======= ==========
+222       22   22222.22
+444       44   44444.44
+555       55   55555.55
+
+1. Display Employee Information
+2. Add Employee
+3. Update Employee Salary
+4. Remove Employee
+0. Exit
+
+Please select from the above options: 2
+
+Adding Employee
+===============
+Enter Employee ID: 666
+Enter Employee Age: 66
+Enter Employee Salary: 66666.66
+
+1. Display Employee Information
+2. Add Employee
+3. Update Employee Salary
+4. Remove Employee
+0. Exit
+
+Please select from the above options: 1
+
+EMP ID  EMP AGE EMP SALARY
+======  ======= ==========
+222       22   22222.22
+666       66   66666.66
+444       44   44444.44
+555       55   55555.55
+
+1. Display Employee Information
+2. Add Employee
+3. Update Employee Salary
+4. Remove Employee
+0. Exit
+
+Please select from the above options: 0
+
+Exiting Employee Data Program. Good Bye!!!
+
+*/
