42c41fd0fa7a3ea15c75c61aecc4390209d6c035
Cameron Gray <cameron.gray@senecacollege.ca>
2020-01-19 16:13:42 -0500

    Workshop-3 Released

---
+++ WS03/at_home/temps.c
@@ -0,0 +1,11 @@
+//==============================================
+// Name:           Full name here
+// Student Number: #########
+// Email:          userID@myseneca.ca
+// Section:        XXX
+// Workshop:       3 (at-home)
+//==============================================
+
+#define _CRT_SECURE_NO_WARNINGS
+
+// Put your code below:
