0e4f54b021b4f23817b36014842cbdf6b969c371
Rob Robson <robert.robson1@senecacollege.ca>
2020-09-17 14:03:30 -0400

    Release for 2207.

---
+++ WS02/at_home/cashRegister.c
@@ -0,0 +1,11 @@
+//==============================================
+// Name:           Full name here
+// Student Number: #########
+// Email:          userID@myseneca.ca
+// Section:        XXX
+// Workshop:       2 (at-home)
+//==============================================
+
+#define _CRT_SECURE_NO_WARNINGS
+
+// Paste your in-lab code below and upgrade it to the at-home specifications
---
+++ WS02/in_lab/cashRegister.c
@@ -0,0 +1,11 @@
+//==============================================
+// Name:           Full name here
+// Student Number: #########
+// Email:          userID@myseneca.ca
+// Section:        XXX
+// Workshop:       2 (in-lab)
+//==============================================
+
+#define _CRT_SECURE_NO_WARNINGS
+
+// Start your code below:
