06f4953398f87790a70e631d590c72dbe3e473d0
Rob Robson <robert.robson1@senecacollege.ca>
2020-09-20 10:55:20 -0400

    Release for 2207.

---
+++ WS03/in_lab/temps.c
@@ -0,0 +1,11 @@
+//==============================================
+// Name:           Full name here
+// Student Number: #########
+// Email:          userID@myseneca.ca
+// Section:        XXX
+// Workshop:       3 (in-lab)
+//==============================================
+
+#define _CRT_SECURE_NO_WARNINGS
+
+// Put your code below:
