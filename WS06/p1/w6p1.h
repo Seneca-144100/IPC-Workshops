11c18670141d971ca3f10c23741dd2486d9ba4fe
Cameron Gray <cameron.gray@senecacollege.ca>
2021-05-24 17:48:47 -0400

    2214-Released

---
+++ WS06/p1/w6p1.h
@@ -0,0 +1,41 @@
+/*
+	==================================================
+	Workshop #6 (Part-1):
+	==================================================
+	Name   :
+	ID     :
+	Email  :
+	Section:
+*/
+
+// ----------------------------------------------------------------------------
+// structures
+
+
+
+
+// ----------------------------------------------------------------------------
+// function prototypes
+
+// 1. Get user input of int type and validate for a positive non-zero number
+//    (return the number while also assigning it to the pointer argument)
+
+
+// 2. Get user input of double type and validate for a positive non-zero number
+//    (return the number while also assigning it to the pointer argument)
+
+
+// 3. Opening Message (include the number of products that need entering)
+
+
+// 4. Get user input for the details of cat food product
+
+
+// 5. Display the formatted table header
+
+
+// 6. Display a formatted record of cat food data
+
+
+// 7. Logic entry point
+
