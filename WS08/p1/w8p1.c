893cfda1c29659232f28bf0d65ba71edfc78d1c4
Cameron Gray <cameron.gray@senecacollege.ca>
2022-03-13 07:51:26 -0400

    WS#8 Released

---
+++ WS08/p1/w8p1.c
@@ -0,0 +1,49 @@
+/*
+*****************************************************************************
+                          Workshop - #8 (P1)
+Full Name  : 
+Student ID#: 
+Email      : 
+Section    : 
+
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+*****************************************************************************
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+
+// System Libraries
+
+
+// User Libraries
+
+
+// 1. Get user input of int type and validate for a positive non-zero number
+//    (return the number while also assigning it to the pointer argument)
+
+
+// 2. Get user input of double type and validate for a positive non-zero number
+//    (return the number while also assigning it to the pointer argument)
+
+
+// 3. Opening Message (include the number of products that need entering)
+
+
+// 4. Get user input for the details of cat food product
+
+
+// 5. Display the formatted table header
+void displayCatFoodHeader(void)
+{
+	printf("SKU         $Price    Bag-lbs Cal/Serv\n");
+	printf("------- ---------- ---------- --------\n");
+}
+
+// 6. Display a formatted record of cat food data
+
+
+// 7. Logic entry point
