0d93ffde096d3c18e531a9d266679701da0c55fe
Cameron Gray <cameron.gray@senecacollege.ca>
2022-06-06 08:35:22 -0400

    WS#6 Released

---
+++ WS06/p2/w6p2.c
@@ -0,0 +1,24 @@
+/*
+*****************************************************************************
+                          Workshop - #6 (P2)
+Full Name  : 
+Student ID#: 
+Email      : 
+Section    : 
+
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+*****************************************************************************
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main(void)
+{
+
+    return 0;
+}
