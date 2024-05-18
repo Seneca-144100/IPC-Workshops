2a8383f2e7ea62cbe23e970926abe07ff2c0301a
RaniaArbash <41278913+RaniaArbash@users.noreply.github.com>
2024-05-18 09:25:02 -0400

    WS#3 has been released

---
+++ WS03/p1/w3p1.c
@@ -0,0 +1,23 @@
+/*/////////////////////////////////////////////////////////////////////////
+                          Workshop - #3 (P1)
+Full Name  : 
+Student ID#: 
+Email      : 
+Section    : 
+
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+/////////////////////////////////////////////////////////////////////////*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    // You must use this variable in #3 data analysis section!
+    const double testValue = 330.99;
+
+
+    return 0;
+}
