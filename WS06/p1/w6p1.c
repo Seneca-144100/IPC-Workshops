c6df8455ce704388698174b1c1bf8d95af5cc1b6
RaniaArbash <41278913+RaniaArbash@users.noreply.github.com>
2024-06-09 20:47:59 -0400

    WS#6 has been released

---
+++ WS06/p1/w6p1.c
@@ -0,0 +1,39 @@
+/*/////////////////////////////////////////////////////////////////////////
+                          Workshop - #6 (P1)
+Full Name  : 
+Student ID#: 
+Email      : 
+Section    : 
+
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+/////////////////////////////////////////////////////////////////////////*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main(void)
+{
+
+    return 0;
+}
+
+/*
+    HELPER: printf formatting statements....
+	=========================================
+	
+    Step #7:
+		printf("Item Priority Financed        Cost\n");
+		printf("---- -------- -------- -----------\n");
+		
+		printf("%3d  %5d    %5c    %11.2lf\n", ...
+		
+
+	Step #8:
+		printf("---- -------- -------- -----------\n");
+		printf("                      $%11.2lf\n\n", ...
+
+*/
---
+++ WS06/p2/w6p2-auto.py
@@ -0,0 +1,21 @@
+import pyautogui as gui
+import time
+
+datafile = 'w6p2-inputs.txt'
+
+# pause 3 seconds while you set focus to the terminal/console window:
+print('Click on the terminal/console window where the automated keyboard data needs to be entered...')
+print('NOTE: You have 5 seconds to do this!')
+time.sleep(5)
+
+# auto type test data from text file:
+with open(datafile, 'r') as tst:
+    time.sleep(1)
+    tst = tst.readlines()
+    for i in range(0, len(tst), 1):
+        print(tst[i])
+        gui.typewrite(tst[i])
+        # Uncomment next line to address slow terminal response
+        #time.sleep(0.5)
+
+print('Auto data input completed.')
